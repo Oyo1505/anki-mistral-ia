e50b8921fb25f80579b9bef3d417e0a1c784b7d6
Oyo1505 <rigoulet.henri.pierre@gmail.com>
2025-03-06 10:52:47 +0100

    fixed added toast

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -11,11 +11,13 @@ import { useState, useTransition } from 'react';
 import Input from './input';
 import ButtonUpload from './button-upload';
 import extractTextFromImage from '@/utils/extract-text-from-image';
+import { toast } from 'react-toastify';
 
 export default function Form() {
 
   const [csvData, setCsvData] = useState<string[][]>([]);
   const [isPending, startTransition] = useTransition();
+
   const levels = [
     { value: "N1 Avancé", label: "N1 - Avancé (Maîtrise complète)" },
     { value: "N2 Pré-avancé", label: "N2 - Pré-avancé (Niveau courant)" },
@@ -42,8 +44,11 @@ export default function Form() {
 
   const onSubmit = async (data: FormDataSchemaType) => {
     try {   
+    
       startTransition(async () => {
+        const id = toast.loading('En cours de génération', { autoClose: false });
         try {
+       
           let res = '';
           if (files?.[0] && (files[0].type === 'image/jpeg' || files[0].type === 'image/png' || files[0].type === 'image/jpg')) {
             const url = URL.createObjectURL(files[0]);
@@ -67,19 +72,26 @@ export default function Form() {
             
             if (answer) {
               setCsvData(answer);
+              toast.success("Génération terminée");
               reset();
             }
           
         } catch (error) {
+          toast.dismiss(id);
+          toast.error("Erreur pendant la génération");
           console.error("Erreur pendant la génération:", error);
         }
+        toast.dismiss(id);
       });
+     
     } catch (error) {
+      toast.error("Erreur lors de la génération des cartes");
       console.error("Erreur générale:", error);
     }
   };
 
   const convert = async (url: string | undefined) => {
+    try{
     if (url?.length) {
       const copyTexts: Array<string> =  [];
       await extractTextFromImage(url).then((txt: string) => {
@@ -87,8 +99,11 @@ export default function Form() {
       })
       return copyTexts.join('\n');
     }
+  } catch (error) {
+    toast.error("Erreur lors de l'extraction du texte");
+    console.error("Erreur lors de l'extraction du texte:", error);
+  }
   };
-
   const convertPdf = async (file: File) => {
     try {
       const formData = new FormData();
@@ -104,6 +119,7 @@ export default function Form() {
       const data = await response.json();
       return data.text;
     } catch (error) {
+      toast.error("Erreur lors de l'extraction du PDF");
       console.error("Erreur lors de l'extraction du PDF:", error);
       return '';
     }
@@ -130,10 +146,11 @@ export default function Form() {
 
   const text = watch('text');
   const isSubmitDisabled = (!text || text.trim() === '') && (!files || files.length === 0);
-
+  const csvDataSuccess = csvData  && csvData.length > 0 &&  !isPending;
+  
   return (
-    <div className='w-full flex flex-col items-start justify-start gap-4'>
-
+    <div className='w-full border-2 md:w-1/2 lg:w-1/3 2xl:w-1/4 p-4 border-white shadow-zinc-600 shadow-2xl rounded-md flex flex-col items-start justify-start gap-4 bg-white'>
+      <h1 className="text-xl w-full text-center font-bold">Générateur de cartes Anki (Basique)</h1>
       <form className="w-full flex flex-col items-start justify-start gap-4" onSubmit={handleSubmit(onSubmit)}>
         <TextArea register={register} errors={errors} id="text" />
         <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-2'>
@@ -158,7 +175,7 @@ export default function Form() {
           {/* <Checkbox label="Générer un CSV ?" handleChangeCheckboxAction={handleChangeCheckboxCsv}/> */}
           <Checkbox label="romanji" title="Voulez-vous inclure les romanji ?" handleChangeCheckboxAction={handleChangeCheckboxRomanji}/>
           <Checkbox label="kanji" title="Voulez-vous inclure les kanji ?" handleChangeCheckboxAction={handleChangeCheckboxKanji} />
-          <Checkbox label="japonais" title="Voulez-vous les énoncés/questions/réponses en japonais ?" handleChangeCheckboxAction={handleChangeCheckboxJapanese} />
+          <Checkbox label="japonais" title="Tout en japonais (énoncés/questions/réponses) ?" handleChangeCheckboxAction={handleChangeCheckboxJapanese} />
         </div>
         <button 
           type='submit' 
@@ -172,14 +189,14 @@ export default function Form() {
          isSubmitDisabled ? 'Veuillez entrer du texte ou ajouter une image' : 'Générer'}
       </button>
       </form>
-      {csvData  && csvData.length > 0 &&  !isPending && 
+      {csvDataSuccess && 
       <>
       <CSVLink className='w-full p-2 rounded-md border-2 border-gray-300 text-center cursor-pointer font-bold' data={csvData}>Télécharger le fichier CSV</CSVLink>
       </>
       }
       <div className='w-full flex items-start justify-between gap-2'>
-        <a className='text-sm border-2 bg-blue-500 text-white  rounded-md p-2' href="https://relieved-circle-d57.notion.site/Tuto-cr-ation-carte-basique-Anki-avec-ChatGPT-19a6823eb75b80e7b564dbc8cf73762d" target="_blank" rel="noopener noreferrer">Tutoriel pour importer dans Anki</a>
-        <a className='text-sm border-2 bg-blue-500 text-white rounded-md p-2' href="https://apps.ankiweb.net/" target="_blank" rel="noopener noreferrer">Telecharger Anki</a>
+        <a className='text-sm text-center border-2 bg-blue-500 text-white  rounded-md p-2' href="https://relieved-circle-d57.notion.site/Tuto-cr-ation-carte-basique-Anki-avec-ChatGPT-19a6823eb75b80e7b564dbc8cf73762d" target="_blank" rel="noopener noreferrer">Tutoriel pour importer dans Anki</a>
+        <a className='text-sm text-center border-2 bg-blue-500 text-white rounded-md p-2' href="https://apps.ankiweb.net/" target="_blank" rel="noopener noreferrer">Télécharger Anki</a>
       </div>
     </div>
   )
